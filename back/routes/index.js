59118ac7b081763fb1520f1ce9a72ab6473d96fc
Simon Renault <simon.renault86@gmail.com>
2026-03-27 13:19:10 +0100

    Changement nom

--- back/routes/index.js
+++ back/routes/index.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get('/', (req, res) => {
     res.render('home.njk', {
-        title: 'Politica — Assemblée Nationale'
+        title: 'Consilium — Assemblée Nationale'
     });
 });
 
