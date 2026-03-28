0ce1977ece145439f7ddc89b957c341684713b5f
Simon Renault <simon.renault86@gmail.com>
2026-03-28 20:38:45 +0100

    ajout fontawesome

--- front/main.js
+++ front/main.js
@@ -1,6 +1,11 @@
 import '@styles/tailwind.css';
 import '@styles/main.scss';
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import HomePage from '@components/HomePage.vue';
 import DeputeInfo from '@components/deputes/DeputeInfo.vue';
 import VotesStats from '@components/deputes/VotesStats.vue';
@@ -8,23 +13,31 @@ import ScrutinInfo from '@components/scrutins/ScrutinInfo.vue';
 import { initDeputes } from '@/helpers/deputes.js';
 import { initPartis } from '@/helpers/partis.js';
 
+library.add(fas, far, fab);
+
+const createVueApp = (component, props = {}) => {
+    const app = createApp(component, props);
+    app.component('FontAwesomeIcon', FontAwesomeIcon);
+    return app;
+};
+
 const mapEl = document.getElementById('assembly-map');
 if (mapEl) {
-    createApp(HomePage).mount(mapEl);
+    createVueApp(HomePage).mount(mapEl);
     initPartis().then(() => initDeputes());
 }
 
-const deputeInfoEl = document.getElementById('depute-info');
+const deputeInfoEl = document.querySelector('depute-info');
 if (deputeInfoEl) {
-    createApp(DeputeInfo, { deputeId: deputeInfoEl.dataset.deputeId }).mount(deputeInfoEl);
+    createVueApp(DeputeInfo, { deputeId: deputeInfoEl.dataset.deputeId }).mount(deputeInfoEl);
 }
 
-const votesStatsEl = document.getElementById('votes-stats');
+const votesStatsEl = document.querySelector('votes-stats');
 if (votesStatsEl) {
-    createApp(VotesStats, { deputeId: votesStatsEl.dataset.deputeId }).mount(votesStatsEl);
+    createVueApp(VotesStats, { deputeId: votesStatsEl.dataset.deputeId }).mount(votesStatsEl);
 }
 
-const scrutinInfoEl = document.getElementById('scrutin-info');
+const scrutinInfoEl = document.querySelector('scrutin-info');
 if (scrutinInfoEl) {
-    createApp(ScrutinInfo, { scrutinUid: scrutinInfoEl.dataset.scrutinUid }).mount(scrutinInfoEl);
+    createVueApp(ScrutinInfo, { scrutinUid: scrutinInfoEl.dataset.scrutinUid }).mount(scrutinInfoEl);
 }
